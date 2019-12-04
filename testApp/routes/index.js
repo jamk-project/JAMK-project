c9988a487da0b6d36480ebf19d489df1203889bc
sudexp <sudexp@icloud.com>
2019-12-04 10:24:00 +0200

    app modified with mysql connection and api routes

--- testApp/routes/index.js
+++ testApp/routes/index.js
@@ -1,8 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+// testing
+router.use(
+  (req, res, next) => {
+    console.log('Request URL:', req.originalUrl);
+    next();
+  }, (req, res, next) => {
+    console.log('Request Type:', req.method);
+    next();
+  });
+
+// GET home page
+router.get('/', (req, res) => {
+  /* res.json({
+    confirmation: 'success',
+    data: 'home page'
+  }); */
   res.render('index', { title: 'Express' });
 });
 
