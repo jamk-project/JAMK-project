7339f1f7cb56140ccac42c4d72cdd2436e046738
sudexp <sudexp@icloud.com>
2020-02-10 09:12:08 +0200

    azure b2c + react + express added

---
+++ testApp_b2c/public/precache-manifest.e1c1630c78d8e8c9e5bab77a14914fa2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c20efeb03935698e1566",
+    "url": "/static/css/main.f37c91e6.chunk.css"
+  },
+  {
+    "revision": "c20efeb03935698e1566",
+    "url": "/static/js/main.c20efeb0.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "97f13f218fd3604d0f96",
+    "url": "/static/css/2.36c37743.chunk.css"
+  },
+  {
+    "revision": "97f13f218fd3604d0f96",
+    "url": "/static/js/2.97f13f21.chunk.js"
+  },
+  {
+    "revision": "19657710cd74a29293f4a7051039357f",
+    "url": "/static/media/icomoon.19657710.ttf"
+  },
+  {
+    "revision": "13efd15705373b9ce7b01efe0b7a3e09",
+    "url": "/static/media/icomoon.13efd157.woff"
+  },
+  {
+    "revision": "8503df9b167e0d47900ec6ed6b411e95",
+    "url": "/static/media/icomoon.8503df9b.svg"
+  },
+  {
+    "revision": "f39e415e020f08a94cc1148df88f0eb9",
+    "url": "/static/media/icomoon.f39e415e.eot"
+  },
+  {
+    "revision": "5110d00f35c46fe589f441e4d0893b46",
+    "url": "/index.html"
+  }
+];
